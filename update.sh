ead071894843918b521e416933d343d87e19c609
github20hao <github20hao@outlook.com>
2020-03-18 14:18:42 +0800

    update 2020-03-18 14:18:42

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# 提交更新至 git 仓库并更新文件至腾讯云存储桶
+#
+# 安装存储桶命令: pip install coscmd
+# https://cloud.tencent.com/document/product/436/10976
+#
+
+cd $(dirname $0)
+
+git add .
+git commit -m "update $(date +'%F %T')"
+git push origin master
+git push gitee master
+
+#GIT_SSH_COMMAND="ssh -i $(pwd)/sshkey/github2_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git push github2 master
+
+coscmd upload config.js config.js
+coscmd upload config.json config.json
